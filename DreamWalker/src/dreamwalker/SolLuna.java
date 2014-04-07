5e1eb86a87e8720479c35a0e79acf00ac450d50c
Enrique Hernandez <enriqueohernandez@gmail.com>
2014-04-07 11:21:06 -0500

    Clase Animacion, SolLuna y SoundClip

---
+++ DreamWalker/src/dreamwalker/SolLuna.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dreamwalker;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author NLCJohn
+ */
+public class SolLuna extends Base {
+    
+    /**
+	 * Metodo constructor que hereda los atributos de la clase <code>Objeto</code>.
+	 * @param posX es la <code>posiscion en x</code> del objeto cuadro.
+	 * @param posY es el <code>posiscion en y</code> del objeto cuadro.
+	 * @param image es la <code>imagen</code> del objeto cuadro.
+	 */
+	public SolLuna(int posX,int posY, Image image){
+            
+            super(posX, posY, image);
+            
+        }
+            
+    
+}
